e3b9dacb6c607336cd12a1f76e339aef06cf3027
Kelver <kelver_kmv@hotmail.com>
2018-03-07 02:33:52 -0400

    Melhoria visual

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,27 +21,41 @@ jQuery(document).ready(function($){
                 var obj = $.parseJSON(data);
                 var index = [];
                 var html = '';
-                $.each(obj.content.legs, function(i, item) {
-                    // oItem = $.parseJSON(item);
-                    if(item.identity.index in index) {
-                        alert(item.identity.index);
-                    }else{
-                        html += '<tr>';
-                        html += '<td>' + item.carrierSummary.airlineName + '</td>';
-                        html += '<td></td>';
-                        html += '<td></td>';
-                        html += '<td>' + item.duration.hours + ':' + item.duration.minutes + '</td>';
-                        html += '<td>' + item.departureLocation.airportCode + ' - ' + item.arrivalLocation.airportCode + '</td>';
-                        html += '<td>' + item.price.formattedPrice + '</td>';
-                        html += '</tr>';
-                        index.push(item.identity.index);
-                    }
-                    console.log(item.identity.index);
-                    console.log(index);
-                    console.log('-------------------');
+                $.each(obj.content.offers, function(i, item) {
+                    var ida = item.legIds[0];
+                    var volta = item.legIds[1];
+
+                    var airportCodeOrigem = item.naturalKey.split('-coach-', 2);
+                    var airportCodeOrigem = airportCodeOrigem['1'].split('-', 1);
+
+                    var airportCodeChegada = item.naturalKey.split(';', 2);
+                    airportCodeChegada = airportCodeChegada[1].split('-coach-', 2);
+                    airportCodeChegada = airportCodeChegada[1].split('-', 1);
+
+                    console.log(ida + ' - ' + volta);
+                    $.each(obj.content.legs, function(i, ob) {
+                        if((ida == ob.identity.naturalKey) && (ob.departureLocation.airportCode == airportCodeOrigem[0].toUpperCase())) {
+                            html += '<tr><td class="view-message"><b>Ida</b></td>';
+                            html += '<td class="view-message">' + ob.carrierSummary.airlineName +'</td>';
+                            html += '<td class="view-message">' + ob.departureTime.time + ' - ' + ob.arrivalTime.time + '</td>';
+                            html += '<td class="view-message">Duração: ' + ob.duration.hours + ':' + ob.duration.minutes + '</td>';
+                            html += '<td class="view-message">' + ob.departureLocation.airportCode + ' - ' + ob.arrivalLocation.airportCode + '</td>';
+                            html += '</tr>';
+                        }else
+                        if((volta == ob.identity.naturalKey) && (ob.departureLocation.airportCode == airportCodeChegada[0].toUpperCase())) {
+                            html += '<tr><td class="view-message"><b>Volta</b></td>';
+                            html += '<td class="view-message">' + ob.carrierSummary.airlineName +'</td>';
+                            html += '<td class="view-message">' + ob.departureTime.time + ' - ' + ob.arrivalTime.time + '</td>';
+                            html += '<td class="view-message">Duração: ' + ob.duration.hours + ':' + ob.duration.minutes + '</td>';
+                            html += '<td class="view-message">' + ob.departureLocation.airportCode + ' - ' + ob.arrivalLocation.airportCode + '</td>';
+                            html += '<td class="view-message" rowspan="2" align="center">Preço<br>' + ob.price.formattedPrice + '</td>';
+                            html += '</tr>';
+                        }
+                    });
                 });
 
                 $('.dados').append(html);
+
             }
         });
 
--- functions/search.php
+++ functions/search.php
@@ -6,49 +6,50 @@
  * Time: 03:26
  */
 
-ini_set('display_errors',1);
-ini_set('display_startup_erros',1);
-error_reporting(E_ALL);
-//if($_POST){
-//	$origem     = $_POST['origem'];
-//	$destino    = $_POST['destino'];
-//	$ida        = $_POST['ida'];
-//	$volta      = $_POST['volta'];
-//	$adultos    = $_POST['adultos'];
-//	$criancas   = $_POST['criancas'];
+//ini_set('display_errors',1);
+//ini_set('display_startup_erros',1);
+//error_reporting(E_ALL);
+if($_POST) {
+	$origem   = $_POST['origem'];
+	$destino  = $_POST['destino'];
+	$ida      = $_POST['ida'];
+	$volta    = $_POST['volta'];
+	$adultos  = $_POST['adultos'];
+	$criancas = $_POST['criancas'];
 
 //https://www.expedia.com.br/Flights-Search?langid=1033&trip=roundtrip&leg1=from:Cuiabá, Brasil (CGQB-Aeroporto Internacional Marechal Rondon),to:Campo Grande, Brasil (CGR-Aeroporto Internacional de Campo Grande),departure:20/03/2018TANYT&leg2=from:Campo Grande, Brasil (CGR-Aeroporto Internacional de Campo Grande),to:Cuiabá, Brasil (CGB-Aeroporto Internacional Marechal Rondon),departure:27/03/2018TANYT&passengers=children:0,adults:1,seniors:0,infantinlap:N&options=cabinclass:economy,sortby:price,carrier:AV&mode=search&paandi=true
-
-$ch = curl_init();
-
-$queryString = array(
-	'langid' => '1033',
-	'trip' => 'roundtrip',
-	'leg1' => 'from:Cuiabá, Brasil (CGQB-Aeroporto Internacional Marechal Rondon),to:Campo Grande, Brasil (CGR-Aeroporto Internacional de Campo Grande),departure:20/03/2018TANYT',
-	'leg2' => 'from:Campo Grande, Brasil (CGR-Aeroporto Internacional de Campo Grande),to:Cuiabá, Brasil (CGB-Aeroporto Internacional Marechal Rondon),departure:27/03/2018TANYT',
-	'passengers' => 'children:0,adults:1,seniors:0,infantinlap:N',
-	'options' => 'cabinclass:economy,sortby:price,carrier:AV',
-	'mode' => 'search',
-	'paandi' => 'true',
-);
+	
+	$ch = curl_init();
+	
+	$queryString = array(
+		'langid'     => '1033',
+		'trip'       => 'roundtrip',
+		'leg1'       => "from:{$origem},to:{$destino},departure:{$ida}TANYT",
+		'leg2'       => "from:{$destino},to:{$origem},departure:{$volta}TANYT",
+		'passengers' => "children:{$criancas},adults:{$adultos},seniors:0,infantinlap:N",
+		'options'    => "cabinclass:economy,sortby:price,carrier:AV",
+		'mode'       => 'search',
+		'paandi'     => 'true',
+	);
 
 // informar URL e outras funções ao CURL
-curl_setopt($ch, CURLOPT_URL, "https://www.expedia.com.br/Flights-Search?" . http_build_query($queryString));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt( $ch, CURLOPT_URL, "https://www.expedia.com.br/Flights-Search?" . http_build_query( $queryString ) );
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
 
 // acessar URL
-$output = curl_exec($ch);
+	$output = curl_exec( $ch );
 // Pegar o código de resposta
-$response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	$response_code = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
 
 // Not found?
-if ($response_code == '404') {
-	echo 'Página não existente';
-} else {
-	$part = explode("/Flight-Search-Paging", $output);
-	$urlJson = explode("'", $part[1]);
-	$json = file_get_contents('https://www.expedia.com.br/Flight-Search-Paging' . $urlJson[0]);
-	echo $json;
+	if ( $response_code == '404' ) {
+		echo 'Página não existente';
+	} else {
+		$part    = explode( "/Flight-Search-Paging", $output );
+		$urlJson = explode( "'", $part[1] );
+		$json    = file_get_contents( 'https://www.expedia.com.br/Flight-Search-Paging' . $urlJson[0] );
+		echo $json;
+	}
 }
 
 
